c7fec23e92e4c68e1fc574f0aeb2dc34979010ca
tripathi-suraj <surajtripathi781@gmail.com>
2024-02-13 20:44:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Decrypt
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/tripathi-suraj/Decrypt)
---
+++ index.js
@@ -0,0 +1,14 @@
+// run `node index.js` in the terminal
+var cryptojs = require('crypto-js');
+
+var decrypt = function (data, gwstoken) {
+  var key = cryptojs.MD5(gwstoken).toString();
+  var dec = cryptojs.AES.decrypt(data, key);
+  var encs = dec.toString(cryptojs.enc.Utf8);
+  console.log(encs);
+};
+
+decrypt(
+  'U2FsdGVkX18sf7Nr1Pd1j1aSZeJEavKq/BuwEhWgq0k=',
+  'ff8726de-e64c-48a0-80b8-c8fb397ce37a'
+);
